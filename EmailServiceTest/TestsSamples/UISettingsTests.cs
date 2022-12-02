88c8ab2cc1ccf957c906953488211fcfaa01dba8
Valera <valera.gs@bk.ru>
2022-12-03 00:40:56 +0300

    Добавлены тесты смены темы.

---
+++ EmailServiceTest/TestsSamples/UISettingsTests.cs
@@ -0,0 +1,37 @@
+﻿using OpenQA.Selenium;
+
+namespace EmailServiceTest.TestsSamples
+{
+    [TestFixture]
+    internal sealed class UISettingsTests : BaseTest
+    {
+        [SetUp]
+        public void BeforeTest()
+        {
+            AuthorizationPageObject authPage = new(_webDriver);
+            MainPageObject mainPage = authPage.Authorize(TestSettings.Login, TestSettings.Password);
+            Assert.IsTrue(mainPage.IsAuthorized(TestSettings.Login));
+        }
+
+        private static By GetThemeXpath(string name)
+            => By.XPath($".//span[text() = '{name}']//parent::div");
+
+        [Test]
+        public void SetDarkMode()
+        {
+            MainPageObject mainPage = new(_webDriver);
+            mainPage.OpenSettings();
+            _webDriver.FindElement(GetThemeXpath("Тёмная тема")).Click();
+            Assert.IsTrue(mainPage.IsDarckModeEnabled());
+        }
+
+        [Test]
+        public void SetLightMode()
+        {
+            MainPageObject mainPage = new(_webDriver);
+            mainPage.OpenSettings();
+            _webDriver.FindElement(GetThemeXpath("Классическая")).Click();
+            Assert.IsFalse(mainPage.IsDarckModeEnabled());
+        }
+    }
+}
